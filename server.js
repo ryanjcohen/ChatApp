41a021b1a6a610c8336bacc99e878a846cd8f5b7
Ryan Cohen <47368809+ryanjcohen@users.noreply.github.com>
2019-11-17 00:59:39 -0500

    Detecting user connecting and disconnecting

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
+var http = require('http').createServer(app);
 var path = require("path");
+var io = require("socket.io")(http);
 
 app.get('/', function(req, res){
 	res.sendFile(path.join(__dirname, '/chat.html'));
@@ -8,4 +10,11 @@ app.get('/', function(req, res){
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(8000);
+io.on('connection', function(socket){
+	console.log("user connected");
+	socket.on('disconnect',function(){
+		console.log('user disconnected');
+	});
+});
+
+http.listen(8000);
